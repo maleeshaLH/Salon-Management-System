459a8d9d2a4588d1e63fcb249072528b3a604b63
maleeshaLH <lhmv114@gmil.com>
2025-02-21 23:42:11 +0530

    create server tables

---
+++ database/service-data.ts
@@ -0,0 +1,60 @@
+import {PrismaClient} from "@prisma/client";
+import {Service} from "../model/service";
+
+const prisma = new PrismaClient()
+
+export async function addService(service :Service){
+    try {
+        await prisma.service.create({
+            data:{
+                serviceId:service.serviceId,
+                name:service.name,
+                duration:service.duration,
+                price:service.price,
+            }
+        })
+
+        console.log("Service Save successfully")
+        return 'Service save Successfully..!'
+    }catch (error){
+        console.log(error)
+    }
+
+}
+
+export async function getAllService(){
+    try {
+        return await prisma.service.findMany()
+
+    }catch (error){
+        return error;
+    }
+}
+
+export async function deleteService(id:string){
+    try {
+        await prisma.service.delete({
+            where:{serviceId: id}
+        })
+        console.log("Service Delete")
+        return 'Service Delete Successfully'
+    }catch (error){
+        return error
+    }
+}
+export async function updateService(id: string, service: Service){
+    try{
+        await prisma.service.update({
+            where:{ serviceId : id},
+            data:{
+                name:service.name,
+                duration:service.duration,
+                price:service.price,
+            }
+        })
+        return 'Service Update success'
+    }catch(err){
+        console.log("error updating service", err);
+        return  err
+    }
+}
---
+++ router/serviceRouter.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import {addAppointment, deleteAppointment, getAllAppointment, updateAppointment} from "../database/appointment-data";
+import {deleteEmployee, updateEmployee} from "../database/employee-date";
+import {Service} from "@prisma/client";
+import {addService, deleteService, getAllService, updateService} from "../database/service-data";
+
+const  router = express.Router();
+
+router.post('/add', async (req,res) =>{
+    res.send(await addService(req.body))
+})
+
+router.get('/getAll', async (req,res) =>{
+
+    try {
+        res.json(await getAllService())
+    }catch (error){
+        console.log("error get All Service "+error)
+    }
+})
+
+router.delete("/delete/:id",async (req,res) => {
+    const  id:string = req.params.id;
+
+    try {
+        await deleteService(id);
+        res.send("Delete Service")
+    }catch (error){
+        console.log("delete Service error"+error)
+    }
+})
+
+router.put("/update/:id",async (req, res) => {
+    const id:string = req.params.id;
+    const service : Service = req.body;
+
+    try{
+        await updateService(id, service);
+        res.send('Service Updated');
+        console.log("Service Update")
+    }catch(err){
+        console.log("error updating service", err);
+    }
+})
+
+export default router;
